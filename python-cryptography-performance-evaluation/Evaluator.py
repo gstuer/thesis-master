fbc3ebe2d49061174eebd806d8ddab2ce0052a16
Moritz Gstuer <moritz@gstuer.com>
2024-05-21 15:45:30 +0200

    Add min/max/mean calculation to python evaluation

--- python-cryptography-performance-evaluation/Evaluator.py
+++ python-cryptography-performance-evaluation/Evaluator.py
@@ -1,5 +1,6 @@
 from Encryptor import RSA2048_SHA256_OAEP,RSA4096_SHA512_OAEP,AES128_CBC,AES256_CBC,AES128_GCM,AES256_GCM
 import os
+import statistics
 
 RANDOM_VALUE_AMOUNT = 100
 RANDOM_BYTES_PER_VALUE = 128
@@ -21,5 +22,12 @@ for encryptor in encryptors:
         decryptTime, decryptOutput = encryptor.decryptTimed(encryptOutput)
         encryptTimes.append(encryptTime)
         decryptTimes.append(decryptTime)
-    results.append({"algorithm": encryptor.identifier, "times_encrypt": encryptTimes, "times_decrypt": decryptTimes})
-print(results)
+    results.append({"identifier": encryptor.identifier, "times_encrypt": encryptTimes, "times_decrypt": decryptTimes})
+
+for algorithm in results:
+    expression = "{identifier}: Encrypt min={enc_min} max={enc_max} avg={enc_avg:.2f} | Decrypt min={dec_min} max={dec_max} avg={dec_avg:.2f}"
+    print(expression.format(
+        identifier=algorithm["identifier"],
+        enc_min=min(algorithm["times_encrypt"]), enc_max=max(algorithm["times_encrypt"]), enc_avg=statistics.fmean(algorithm["times_encrypt"]),
+        dec_min=min(algorithm["times_decrypt"]), dec_max=max(algorithm["times_decrypt"]), dec_avg=statistics.fmean(algorithm["times_decrypt"])
+    ))
