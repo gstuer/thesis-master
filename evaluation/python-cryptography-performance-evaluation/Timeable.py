c4933e8981013997e6b64bb043fb6c1300cc6739
Moritz Gstuer <moritz@gstuer.com>
2025-02-02 09:29:02 +0100

    Move evaluation programs to implementation repo

--- evaluation/python-cryptography-performance-evaluation/Timeable.py
+++
@@ -1,9 +0,0 @@
-import time
-import timeit
-
-class Timeable:
-    def executeTimed(self, function):
-        timer = timeit.Timer(function, timer=time.perf_counter_ns)
-        # TODO Implement free method to reset child class (free memory etc.) and then average multiple runs
-        nanoseconds = timer.timeit(number=1)
-        return nanoseconds
